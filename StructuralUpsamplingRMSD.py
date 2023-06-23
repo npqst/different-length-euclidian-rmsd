3d6354d5a208e4978b5f3d2f7fdafe4c4aaf82dc
Nele Quast <nele.pauline@gmail.com>
2023-06-23 13:29:46 +0100

    Spline interpolation added

--- StructuralUpsamplingRMSD.py
+++ StructuralUpsamplingRMSD.py
@@ -6,22 +6,24 @@ from scipy.interpolate import splprep, splev
 class StructuralUpsamplingRMSD():
     def __init__(self):
         return
-    
+
     def calc_upsampled_rmsd(self, x, y, upsample_to=20):
 
         upsampled_x = self._upsample(x, new_length=upsample_to)
-        upsampled_y = self._upsample(y, new_length=upsample_to)    
+        upsampled_y = self._upsample(y, new_length=upsample_to)
 
         rmsd = self._rmsd(upsampled_x, upsampled_y)
 
-        return rmsd, upsampled_x
-    
+        return rmsd
 
     def _upsample(self, input_series, new_length=20):
-        tck, _ = splprep(input_series.T, u=np.asarray([*range(len(input_series))]), s=0)
+        tck, _ = splprep(
+                        input_series.T,
+                        u=np.asarray([*range(len(input_series))]),
+                        s=0)
         upsampled = splev(np.linspace(0, len(input_series)-1, new_length), tck)
         return np.asarray(upsampled).T
-    
+
     @staticmethod
     def _rmsd(x, y):
         assert x.shape == y.shape, f"""Shape of x ({x.shape})
@@ -29,4 +31,3 @@ class StructuralUpsamplingRMSD():
                                     identical for RMSD"""
         rmsd = np.sqrt(np.sum(np.sum((x-y)**2, axis=-1))/len(x))
         return rmsd
-
---
+++ tests/testStructuralUpsamplingRMSD.py
@@ -0,0 +1,79 @@
+import unittest
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from StructuralUpsamplingRMSD import StructuralUpsamplingRMSD
+
+
+class TestStructuralUpsamplingRMSD(unittest.TestCase):
+    def test_rmsd(self):
+        x = np.random.rand(12, 3)
+        y = np.zeros_like(x)
+        rmsd = np.sqrt(np.sum(np.sum(x**2, axis=-1))/12)
+
+        upsampling_rmsd = StructuralUpsamplingRMSD()
+
+        self.assertEqual(rmsd, upsampling_rmsd._rmsd(x, y))
+
+    def test_upsample(self):
+        x = np.linspace(1, 5, 10) + np.sin(np.linspace(1, 5, 10))
+        y = np.linspace(1, 10, 10) + np.sin(2*np.linspace(1, 10, 10))
+        z = np.linspace(-2, 30, 10) + np.sin(1.5*np.linspace(-2, 30, 10))
+
+        cood = np.stack([x, y, z], axis=-1)
+
+        upsampling_rmsd = StructuralUpsamplingRMSD()
+        upsampled_cood = upsampling_rmsd._upsample(cood, new_length=20)
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+
+        # Plot the line
+        ax.plot3D(cood.T[0], cood.T[1], cood.T[2], 'b')
+        ax.plot3D(
+            upsampled_cood.T[0],
+            upsampled_cood.T[1],
+            upsampled_cood.T[2],
+            'r'
+            )
+
+        # Set labels for the axes
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.set_zlabel('Z')
+        plt.show()
+        return
+
+    def test_calc_upsampled_rmsd(self):
+        x = np.linspace(1, 5, 10) + np.sin(np.linspace(1, 5, 10))
+        y = np.linspace(1, 10, 10) + np.sin(2*np.linspace(1, 10, 10))
+        z = np.linspace(-2, 30, 10) + np.sin(1.5*np.linspace(-2, 30, 10))
+
+        cood = np.stack([x, y, z], axis=-1)
+        base_cood = np.random.rand(*cood.shape)
+
+        upsampling_rmsd = StructuralUpsamplingRMSD()
+
+        self.assertAlmostEqual(
+            upsampling_rmsd._rmsd(cood, base_cood),
+            upsampling_rmsd.calc_upsampled_rmsd(
+                                    cood,
+                                    base_cood,
+                                    upsample_to=x.shape[-1]
+                                    ), places=6)
+
+        self.assertTrue(
+            np.abs(
+                upsampling_rmsd.calc_upsampled_rmsd(
+                                    cood,
+                                    base_cood,
+                                    upsample_to=x.shape[-1]
+                                    )
+                - upsampling_rmsd.calc_upsampled_rmsd(
+                                    cood,
+                                    base_cood,
+                                    upsample_to=20
+                                    ))
+            < 0.5
+                         )
