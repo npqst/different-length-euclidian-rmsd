0346e199dd9e26f7a2499d9dccfb397e665784f0
Nele Quast <nele.pauline@gmail.com>
2023-06-27 15:56:17 +0100

    added loop examples with tests

--- tests/testDynamicTimeWarDistance.py
+++ tests/testDynamicTimeWarDistance.py
@@ -55,3 +55,56 @@ class TestDynamicTimeWarpDistance(unittest.TestCase):
         for i in range(len(x) - 1):
             self.assertEqual(b[i], (i, i))
         self.assertAlmostEqual(a, rmsd, places=10)
+
+    def test_dp_rmsd_equivalence_on_examples(self):
+        from Bio.PDB import PDBParser
+
+        p = PDBParser()
+        pdb7650 = p.get_structure(
+            "7650",
+            """different-length-euclidian-rmsd/examples/
+            1279054_1_Paired_All_7650.pdb""",
+        )
+        pdb7744 = p.get_structure(
+            "7744",
+            """different-length-euclidian-rmsd/examples/
+            1279059_1_Paired_All_7744.pdb""",
+        )
+        pdb11883 = p.get_structure(
+            "11883",
+            """different-length-euclidian-rmsd/examples/
+            1287167_1_Paired_All_11883.pdb""",
+        )
+
+        ca_7650 = np.asarray(
+            [x.coord for x in pdb7650.get_atoms() if x.id == "CA"]
+        )
+        ca_7744 = np.asarray(
+            [x.coord for x in pdb7744.get_atoms() if x.id == "CA"]
+        )
+        ca_11883 = np.asarray(
+            [x.coord for x in pdb11883.get_atoms() if x.id == "CA"]
+        )
+        dtw = DynamicTimeWarpDistance()
+
+        pairs = [(ca_7650, ca_7744), (ca_11883, ca_7744), (ca_7650, ca_11883)]
+
+        for cood_1, cood_2 in pairs:
+            dtw_dist, dtw_path = dtw.dtw_dist(
+                cood_1, cood_2, get_alignment=True
+            )
+            dist_matrix = dtw._calc_dist_matrix(cood_1, cood_2)
+            rmsd = np.sqrt(
+                np.sum(np.sum((cood_1 - cood_2) ** 2, axis=-1)) / len(cood_1)
+            )
+            diag_dist_mat_sum = np.sqrt(
+                np.sum(dist_matrix * np.eye(len(cood_1))) / len(cood_1)
+            )
+
+            self.assertAlmostEqual(rmsd, diag_dist_mat_sum)
+
+            diagonal_path = [(i, i) for i in range(len(cood_1) - 1)]
+            if dtw_path != diagonal_path:
+                self.assertTrue(rmsd >= dtw_dist)
+            else:
+                self.assertAlmostEqual(rmsd, dtw_dist)
